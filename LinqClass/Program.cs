1c6c8d2a906e63f4c70e286ae8f788d16676b4cd
Robert Rice <rrice1291@gmail.com>
2019-03-12 18:25:26 -0500

    Add project files.

---
+++ LinqClass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinqClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
