999781a7550f82a09136d3e7587bac3285f76313
Robert Rice <rrice1291@gmail.com>
2019-03-12 21:33:01 -0500

    finished first linq exercise

--- LinqClass/Program.cs
+++ LinqClass/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LinqClass
 {
@@ -6,7 +8,36 @@ namespace LinqClass
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var goodSongs = new List<Song> () ;
+            var allSongs = new List<Song> ();
+
+            var funSong = new Song("Imagine Dragons", "Bleeding Out");
+
+            allSongs.Add(funSong);
+            allSongs.Add(new Song("Imagine Dragons", "Fire"));
+            allSongs.Add(new Song("Imagine Dragons", "Whatever It Takes"));
+            allSongs.Add(new Song("Imagine Dragons", "Friction"));
+            allSongs.Add(new Song("Imagine Dragons", "Amsterdam"));
+            allSongs.Add(new Song("Imagine Dragons", "Shots"));
+            allSongs.Add(new Song("Imagine Dragons", "Believer"));
+            allSongs.Add(new Song("Imagine Dragons", "Demons"));
+            allSongs.Add(new Song("Imagine Dragons", "Walking The Wire"));
+            allSongs.Add(new Song("Imagine Dragons", "Radioactive"));
+            allSongs.Add(new Song("Imagine Dragons", "It's Time"));
+            allSongs.Add(new Song("Imagine Dragons", "Is It Time?"));
+            allSongs.Add(new Song("Imagine Dragons", "It Is Time"));
+            allSongs.Add(new Song("Elton John", "Tiny Dancer"));
+
+            goodSongs = (from Song in allSongs
+                        where Song.Artist != "Imagine Dragons"
+                        select Song).ToList();
+
+            foreach (var song in goodSongs)
+            {
+                Console.WriteLine($"{song.Artist} : {song.Name}");
+            }
+            Console.ReadLine();
+
         }
     }
 }
